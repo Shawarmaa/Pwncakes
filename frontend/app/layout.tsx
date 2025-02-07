4a0c3b53eceb20905df2564748593b4f1fee72ea
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-07 23:26:08 +0000

    boilerplate frontend code

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Inter } from 'next/font/google';
+
+
+const inter = Inter({ subsets: ['latin'], weight: '400' });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.className} antialiased`}>
+      <head>
+        <title>Pwncakes</title>
+      </head>
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
+
